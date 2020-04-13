c1bec9cbe87034afe4112d3fb2856445ede6818b
Noam Raz <noamraz8@gmail.com>
2020-04-13 22:43:28 +0300

    Add test for printing empty board

--- tests/test_board.c
+++ tests/test_board.c
@@ -1,8 +1,10 @@
 #include "board.h"
 
 #include <assert.h>
+#include <stdio.h>
+#include <string.h>
 
-void test_board_access() {
+static void test_board_access() {
     board_t board;
 
     assert(board_init(&board, 2, 5));
@@ -13,7 +15,41 @@ void test_board_access() {
     assert(board.cells[13].value == 17);
 }
 
+static void test_board_print_empty() {
+    board_t board;
+    FILE* stream;
+    char buf[1024] = {0};
+    const char* expected = "-------------------------------------------\n"
+                           "|                    |                    |\n"
+                           "|                    |                    |\n"
+                           "-------------------------------------------\n"
+                           "|                    |                    |\n"
+                           "|                    |                    |\n"
+                           "-------------------------------------------\n"
+                           "|                    |                    |\n"
+                           "|                    |                    |\n"
+                           "-------------------------------------------\n"
+                           "|                    |                    |\n"
+                           "|                    |                    |\n"
+                           "-------------------------------------------\n"
+                           "|                    |                    |\n"
+                           "|                    |                    |\n"
+                           "-------------------------------------------\n";
+
+    assert(board_init(&board, 2, 5));
+    stream = tmpfile();
+    assert(stream);
+
+    board_print(&board, stream);
+    rewind(stream);
+    fread(buf, 1, sizeof(buf), stream);
+    fputs(buf, stderr);
+
+    assert(!strcmp(buf, expected));
+}
+
 int main() {
     test_board_access();
+    test_board_print_empty();
     return 0;
 }
