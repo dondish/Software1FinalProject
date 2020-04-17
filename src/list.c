0f361c7f86acc7c001bba6d8d895187c3823638e
Noam Raz <noamraz8@gmail.com>
2020-04-17 19:06:55 +0300

    Define list API

--- src/list.c
+++ src/list.c
@@ -1,1 +1,63 @@
 #include "list.h"
+
+#include "bool.h"
+#include "checked_alloc.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+void list_init(list_t* list) {
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+void list_destroy(list_t* list, list_item_dtor_t dtor) {
+    list_node_t* iter = list->head;
+
+    while (iter) {
+        list_node_t* next = iter->next;
+        dtor(iter->value);
+        free(iter);
+        iter = next;
+    }
+
+    /* Fail fast if we accidentally attempt to use the list later. */
+    memset(list, 0, sizeof(list_t));
+}
+
+bool_t list_is_empty(const list_t* list) { return !list->head; }
+
+void list_push(list_t* list, void* value) {
+    list_node_t* node = checked_malloc(sizeof(list_node_t));
+
+    node->prev = list->tail;
+    node->next = NULL;
+    node->value = value;
+
+    if (list_is_empty(list)) {
+        list->head = node;
+    } else {
+        list->tail->next = node;
+    }
+
+    list->tail = node;
+}
+
+void* list_pop(list_t* list) {
+    list_node_t* node = list->tail;
+    void* value;
+
+    if (!node) {
+        return NULL;
+    }
+
+    value = node->value;
+
+    list->tail = node->prev;
+    if (!node->prev) {
+        list->head = NULL;
+    }
+
+    free(node);
+    return value;
+}
--- src/list.h
+++ src/list.h
@@ -1,4 +1,48 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include "bool.h"
+
+typedef struct list_node list_node_t;
+
+struct list_node {
+    list_node_t* prev;
+    list_node_t* next;
+
+    void* value;
+};
+
+typedef struct list {
+    list_node_t* head;
+    list_node_t* tail;
+} list_t;
+
+typedef void (*list_item_dtor_t)(void* value);
+
+/**
+ * Initialize a new, empty list.
+ */
+void list_init(list_t* list);
+
+/**
+ * Deallocate all items in `list`, invoking `dtor` on each value first.
+ */
+void list_destroy(list_t* list, list_item_dtor_t dtor);
+
+/**
+ * Check whether `list` is empty.
+ */
+bool_t list_is_empty(const list_t* list);
+
+/**
+ * Append `value` to the end of `list`.
+ */
+void list_push(list_t* list, void* value);
+
+/**
+ * Remove the last node in `list`, returning its value or `NULL` if the list is
+ * empty.
+ */
+void* list_pop(list_t* list);
+
 #endif
