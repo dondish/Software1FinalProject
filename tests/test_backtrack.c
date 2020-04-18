26478646857638021922e291d35c9bf857238b3c
Noam Raz <noamraz8@gmail.com>
2020-04-18 14:57:45 +0300

    Add additional solution counting test

--- tests/test_backtrack.c
+++ tests/test_backtrack.c
@@ -4,12 +4,26 @@
 #include <assert.h>
 #include <stdio.h>
 
+#define VAL(row, col) board_access(&board, row, col)->value
+
 int main() {
     board_t board;
     board_init(&board, 2, 2);
     assert(num_solutions(&board) == 288); /* According to wikipedia */
-    board_access(&board, 0, 0)->value = 1;
-    board_access(&board, 1, 1)->value = 1;
+
+    VAL(0, 0) = 1;
+    VAL(0, 1) = 1;
     assert(num_solutions(&board) == 0);
+
+    VAL(0, 1) = 2;
+    VAL(0, 2) = 3;
+    VAL(0, 3) = 4;
+    VAL(1, 0) = 3;
+    VAL(1, 1) = 4;
+    VAL(1, 2) = 1;
+    VAL(1, 3) = 2;
+    VAL(2, 1) = 1;
+    assert(num_solutions(&board) == 2);
+
     return 0;
 }
