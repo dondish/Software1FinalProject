74ec2aa9e8fe3bb49be7f926dd4a76dca77922bc
Noam Raz <noamraz8@gmail.com>
2020-04-20 21:29:01 +0300

    Refactor parser
    The code is now much more maintainable and supports all commands.
    In addition, validation of integer values is no longer performed as it
    is not the parser's responsibility.

--- src/parser.c
+++ src/parser.c
@@ -8,6 +8,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * Allowed mode flags
+ */
+enum {
+    AM_INIT = (1 << GM_INIT),
+    AM_EDIT = (1 << GM_EDIT),
+    AM_SOLVE = (1 << GM_SOLVE),
+    AM_ALL = AM_INIT | AM_EDIT | AM_SOLVE
+};
+
+/**
+ * Argument payload type.
+ */
+typedef enum {
+    PT_NONE,
+    PT_STR,
+    PT_OPT_STR,
+    PT_BOOL,
+    PT_FLOAT,
+    PT_INT2,
+    PT_INT3
+} command_arg_type_t;
+
+/**
+ * Describes a command, including name, allowed modes and argument types.
+ */
+typedef struct {
+    const char* name;
+    command_type_t type;
+    unsigned allowed_modes;
+    command_arg_type_t payload_type;
+} command_desc_t;
+
 void command_arg_str_destroy(command_arg_str_t* arg) { free(arg->str); }
 
 /**
@@ -33,20 +66,6 @@ static bool_t extract_arguments(char** args, size_t size) {
 }
 
 /**
- * Consume the rest of the current line in `stream`, returning `FALSE` on IO
- * error.
- */
-static bool_t read_rest_of_line(FILE* stream) {
-    char c;
-
-    do {
-        c = fgetc(stream);
-    } while (c != '\n' && c != EOF);
-
-    return !ferror(stream);
-}
-
-/**
  * Copy the contents of `str` into a new heap-allocated string.
  */
 static char* duplicate_str(const char* str) {
@@ -61,10 +80,152 @@ static char* duplicate_str(const char* str) {
     return dup;
 }
 
+/**
+ * Attempt to parse `count` integers from `strs`, storing the results to `vals`.
+ */
+static bool_t parse_ints(char** strs, int* vals, int count) {
+    int i;
+    for (i = 0; i < count; i++) {
+        if (sscanf(strs[i], "%d", &vals[i]) < 1) {
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+
+static parser_error_codes_t parse_arg_payload(command_arg_type_t type,
+                                              command_arg_t* arg) {
+    switch (type) {
+    case PT_NONE:
+        if (!extract_arguments(NULL, 0)) {
+            return P_INVALID_NUM_OF_ARGS;
+        }
+        break;
+    case PT_STR: {
+        char* str;
+        if (!extract_arguments(&str, 1)) {
+            return P_INVALID_NUM_OF_ARGS;
+        }
+        arg->str.str = duplicate_str(str);
+        break;
+    }
+    case PT_OPT_STR: {
+        char* str = strtok_ws(NULL); /* May be null */
+
+        if (strtok_ws(NULL) != NULL) {
+            return P_INVALID_NUM_OF_ARGS;
+        }
+        arg->str.str = duplicate_str(str);
+        break;
+    }
+    case PT_BOOL: {
+        char* str_arg;
+        int val;
+
+        if (!extract_arguments(&str_arg, 1)) {
+            return P_INVALID_NUM_OF_ARGS;
+        }
+
+        if (sscanf(str_arg, "%d", &val) < 1 || (val != 0 && val != 1)) {
+            return P_INVALID_ARGUMENTS;
+        }
+
+        arg->bool.val = (bool_t)val;
+        break;
+    }
+    case PT_FLOAT: {
+        char* str_arg;
+        float val;
+
+        if (!extract_arguments(&str_arg, 1)) {
+            return P_INVALID_NUM_OF_ARGS;
+        }
+
+        if (sscanf(str_arg, "%f", &val) < 1) {
+            return P_INVALID_ARGUMENTS;
+        }
+
+        arg->onefloat.val = val;
+        break;
+    }
+    case PT_INT2: {
+        char* str_args[2];
+        int vals[2];
+
+        if (!extract_arguments(str_args, 2)) {
+            return P_INVALID_NUM_OF_ARGS;
+        }
+
+        if (!parse_ints(str_args, vals, 2)) {
+            return P_INVALID_ARGUMENTS;
+        }
+
+        arg->twointegers.i = vals[0];
+        arg->twointegers.j = vals[1];
+        break;
+    }
+    case PT_INT3: {
+        char* str_args[3];
+        int vals[3];
+
+        if (!extract_arguments(str_args, 3)) {
+            return P_INVALID_NUM_OF_ARGS;
+        }
+
+        if (!parse_ints(str_args, vals, 3)) {
+            return P_INVALID_ARGUMENTS;
+        }
+
+        arg->threeintegers.i = vals[0];
+        arg->threeintegers.j = vals[1];
+        arg->threeintegers.k = vals[2];
+        break;
+    }
+    }
+    return P_SUCCESS;
+}
+
+/**
+ * Consume the rest of the current line in `stream`, returning `FALSE` on IO
+ * error.
+ */
+static bool_t read_rest_of_line(FILE* stream) {
+    char c;
+
+    do {
+        c = fgetc(stream);
+    } while (c != '\n' && c != EOF);
+
+    return !ferror(stream);
+}
+
 parser_error_codes_t parse_line(FILE* stream, command_t* cmd,
                                 game_mode_t mode) {
+    static const command_desc_t descs[] = {
+        {"solve", CT_SOLVE, AM_ALL, PT_STR},
+        {"edit", CT_EDIT, AM_ALL, PT_OPT_STR},
+        {"mark_errors", CT_MARK_ERRORS, AM_SOLVE, PT_BOOL},
+        {"print_board", CT_PRINT_BOARD, AM_EDIT | AM_SOLVE, PT_NONE},
+        {"set", CT_SET, AM_EDIT | AM_SOLVE, PT_INT3},
+        {"validate", CT_VALIDATE, AM_EDIT | AM_SOLVE, PT_NONE},
+        {"guess", CT_GUESS, AM_SOLVE, PT_FLOAT},
+        {"generate", CT_GENERATE, AM_EDIT, PT_INT2},
+        {"undo", CT_UNDO, AM_EDIT | AM_SOLVE, PT_NONE},
+        {"redo", CT_REDO, AM_EDIT | AM_SOLVE, PT_NONE},
+        {"save", CT_SAVE, AM_EDIT | AM_SOLVE, PT_STR},
+        {"hint", CT_HINT, AM_SOLVE, PT_INT2},
+        {"guess_hint", CT_GUESS_HINT, AM_SOLVE, PT_INT2},
+        {"num_solutions", CT_NUM_SOLUTIONS, AM_EDIT | AM_SOLVE, PT_NONE},
+        {"autofill", CT_AUTOFILL, AM_SOLVE, PT_NONE},
+        {"reset", CT_RESET, AM_EDIT | AM_SOLVE, PT_NONE},
+        {"exit", CT_EXIT, AM_ALL, PT_NONE},
+    };
+
+    unsigned mode_mask = 1 << mode;
+
     char line[258] = {0};
-    char* curr;
+    char* name;
+    size_t i;
 
     fgets(line, 258, stream);
 
@@ -76,129 +237,27 @@ parser_error_codes_t parse_line(FILE* stream, command_t* cmd,
         return read_rest_of_line(stream) ? P_LINE_TOO_LONG : P_IO;
     }
 
-    curr = strtok_ws(line);
+    name = strtok_ws(line);
 
-    if (curr == NULL)
+    if (!name) {
         return P_IGNORE;
-
-    if (!strcmp(curr, "solve")) {
-        command_arg_str_t arg;
-        char* temparg;
-
-        cmd->type = CT_SOLVE;
-
-        if (!extract_arguments(&temparg, 1)) {
-            return P_INVALID_NUM_OF_ARGS;
-        }
-
-        arg.str = duplicate_str(temparg);
-        cmd->arg.str = arg;
-    } else if (!strcmp(curr, "edit")) {
-        command_arg_str_t arg;
-
-        /* Note: not using extract_arguments as filename is optional. */
-        char* temparg = strtok_ws(NULL);
-
-        cmd->type = CT_EDIT;
-
-        arg.str = duplicate_str(temparg);
-        cmd->arg.str = arg;
-    } else if (!strcmp(curr, "mark_errors")) {
-        command_arg_bool_t boolt;
-        int x;
-        char* temparg;
-
-        cmd->type = CT_MARK_ERRORS;
-
-        if (mode != GM_SOLVE)
-            return P_INVALID_MODE;
-
-        if (!extract_arguments(&temparg, 1)) {
-            return P_INVALID_NUM_OF_ARGS;
-        } else if (!sscanf(temparg, "%d", &x) || x > 1 || x < 0) {
-            return P_INVALID_ARGUMENTS;
-        } else {
-            boolt.val = x;
-        }
-
-        cmd->arg.bool = boolt;
-    } else if (!strcmp(curr, "print_board")) {
-        cmd->type = CT_PRINT_BOARD;
-        if (mode == GM_INIT)
-            return P_INVALID_MODE;
-    } else if (!strcmp(curr, "set")) {
-        command_arg_three_int_t arg;
-        char* strparams[3];
-        int params[3];
-        int i;
-
-        cmd->type = CT_SET;
-
-        if (mode == GM_INIT)
-            return P_INVALID_MODE;
-
-        if (!extract_arguments(strparams, 3)) {
-            return P_INVALID_NUM_OF_ARGS;
-        }
-
-        for (i = 0; i < 3; i++) {
-            if (!sscanf(strparams[i], "%d", &params[i])) {
-                return P_INVALID_ARGUMENTS;
-            }
-        }
-
-        arg.i = params[0];
-        arg.j = params[1];
-        arg.k = params[2];
-        cmd->arg.threeintegers = arg;
-    } else if (!strcmp(curr, "validate")) {
-        cmd->type = CT_VALIDATE;
-        if (mode == GM_INIT)
-            return P_INVALID_MODE;
-    } else if (!strcmp(curr, "guess")) {
-        command_arg_float_t arg;
-        char* temparg;
-
-        cmd->type = CT_GUESS;
-
-        if (mode != GM_SOLVE)
-            return P_INVALID_MODE;
-
-        if (!extract_arguments(&temparg, 1)) {
-            return P_INVALID_NUM_OF_ARGS;
-        }
-
-        if (!sscanf(temparg, "%f", &arg.val)) {
-            return P_INVALID_ARGUMENTS;
-        }
-
-        cmd->arg.onefloat = arg;
-    } else if (!strcmp(curr, "generate")) {
-        command_arg_two_int_t arg;
-        char* args[2];
-
-        cmd->type = CT_GENERATE;
-
-        if (mode != GM_EDIT)
-            return P_INVALID_MODE;
-
-        if (!extract_arguments(args, 2)) {
-            return P_INVALID_NUM_OF_ARGS;
-        }
-
-        if (!sscanf(args[0], "%d", &arg.i) || arg.i < 0)
-            return P_INVALID_ARGUMENTS;
-        if (!sscanf(args[1], "%d", &arg.j) || arg.j <= 0)
-            return P_INVALID_ARGUMENTS;
-
-        cmd->arg.twointegers = arg;
-    } else {
-        return P_INVALID_COMMAND_NAME;
     }
 
-    if (strtok_ws(NULL) != NULL) {
-        return P_INVALID_NUM_OF_ARGS;
+    for (i = 0; i < sizeof(descs) / sizeof(descs[0]); i++) {
+        const command_desc_t* desc = &descs[i];
+
+        if (strcmp(name, desc->name)) {
+            continue;
+        }
+
+        cmd->type = desc->type;
+
+        if (!(desc->allowed_modes & mode_mask)) {
+            return P_INVALID_MODE;
+        }
+
+        return parse_arg_payload(desc->payload_type, &cmd->arg);
     }
 
-    return P_SUCCESS;
+    return P_INVALID_COMMAND_NAME;
 }
