3be74339929d392a0df52e0e36799f65262c23a3
Noam Raz <noamraz8@gmail.com>
2020-04-21 23:13:45 +0300

    Tweak LP interface

--- src/lp.h
+++ src/lp.h
@@ -2,9 +2,35 @@
 #define LP_H
 
 #include "board.h"
+#include "bool.h"
 
-typedef enum lp_status { LP_SUCCESS, LP_INFEASIBLE, LP_GUROBI_ERR } lp_status_t;
+/**
+ * Opaque type representing a linear programming environment.
+ */
+typedef void* lp_env_t;
 
-lp_status_t solve_ilp(board_t* board);
+/**
+ * Linear programming status codes
+ */
+typedef enum lp_status {
+    LP_SUCCESS,    /* Solving succeeded */
+    LP_INFEASIBLE, /* Board is infeasible */
+    LP_GUROBI_ERR  /* Internal gurobi error */
+} lp_status_t;
+
+/**
+ * Initialize a new linear programming environment.
+ */
+bool_t lp_env_init(lp_env_t* env);
+
+/**
+ * Destroy a linear programming environment.
+ */
+void lp_env_destroy(lp_env_t env);
+
+/**
+ * Attempt to solve `board` in-place using ILP
+ */
+lp_status_t lp_solve_ilp(lp_env_t env, board_t* board);
 
 #endif
