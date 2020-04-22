a0cc107843d4097189f2ffa0824d73559ce3c35c
Noam Raz <noamraz8@gmail.com>
2020-04-22 16:52:45 +0300

    Update LP test

--- tests/test_lp.c
+++ tests/test_lp.c
@@ -6,12 +6,24 @@
 int main() {
     board_t board;
     lp_env_t env;
+    int block_size;
+    int i, j;
 
-    board_init(&board, 2, 2);
+    board_init(&board, 4, 4);
     board_access(&board, 0, 0)->value = 1;
     assert(lp_env_init(&env) == LP_SUCCESS);
 
     assert(lp_solve_ilp(env, &board) == LP_SUCCESS);
+
+    board_print(&board, stderr, FALSE);
+    assert(board_is_legal(&board));
+
+    block_size = board_block_size(&board);
+    for (i = 0; i < block_size; i++) {
+        for (j = 0; j < block_size; j++) {
+            assert(!cell_is_empty(board_access(&board, i, j)));
+        }
+    }
 
     lp_env_destroy(env);
 
